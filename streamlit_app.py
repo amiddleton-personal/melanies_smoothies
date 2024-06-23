57af959f277666f92251e09e62e6a9b9c64ee165
amiddleton-personal <andrew@themiddletons.uk>
2024-06-23 19:41:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,6 +30,8 @@ if ingredients_list:
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order) values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
 
     st.write(my_insert_stmt)
+
+
     
     time_to_insert = st. button ('Submit Order')
     
@@ -37,3 +39,8 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 	    
         st.success('Your Smoothie is ordered!', icon="✅")
+
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fv_df = st. dataframe(data=fruityvice_response.json(), use_container_width=True)
