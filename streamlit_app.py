427e1bbe1df2e25b10ada243334dad9ec2a481fc
amiddleton-personal <andrew@themiddletons.uk>
2024-06-27 11:29:38 +0100

    Update streamlit_app.py lesson 10 changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 
 st.title(":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
@@ -24,6 +25,8 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     st.write(ingredients_string)
 
 
@@ -41,6 +44,4 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
 
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+
